21f21076a4823bf6196530a8b257dbfcff7c37a1
rishusiva <rishusiva@gmail.com>
2021-11-14 11:10:47 +0530

    mask preprocessing commit

---
+++ Code/mask_preprocessing.py
@@ -0,0 +1,43 @@
+from modules import *
+from mask_mapping import *
+from pixels_count import *
+
+
+mask_dir = 'Data/Masks'
+mask_ext = '-mask.png'
+new_mask_ext = '-mask-mod.png'
+dat = [] #list that will hold information on the masks
+
+for img_name in img_names:
+    name = mask_dir + img_name
+    # importing the image  
+    mask = Image.open(name + mask_ext)
+
+    # converting mask
+    mask = np.array(mask)#convert to numpy
+    new_mask = map_mask(mask, mask_lib)#map values
+    
+    #update dataframe
+    name = img_name.split('-')  
+    d = [img_name, 
+         name[0][1:],  #patch id
+         name[1][0:4], #year
+         name[1][4:6], #month
+         name[1][6:8], #day
+         name[1][8:10]]#hour
+    
+    counts = bincount_2d(new_mask, n_colors) #values counts of the class of ice over all pixels in the image
+    d.extend(counts)
+    dat.append(d)
+    
+    # exporting the image 
+    new_mask = Image.fromarray(new_mask)#convert back to image
+    new_mask.save('./' + img_name + new_mask_ext, 'PNG') 
+
+mask_dir = './'#update mask directory and extension
+mask_ext= new_mask_ext
+
+#create dataframe of mask information
+mask_df = pd.DataFrame(dat, columns = ['name', 'patch_id', 'year', 'month', 'day', 'hour', 
+                            'conc_0', 'conc_1', 'conc_2', 'conc_3', 'conc_4', 'conc_5', 'conc_6',  
+                            'conc_land'])
